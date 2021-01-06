249a3eae88472037ef8548fb2be19b2d48239693
kunxin-chor <chorkunxin@yahoo.com>
2021-01-06 13:03:25 +0000

    Added write to json

---
+++ read_json.py
@@ -0,0 +1,13 @@
+import json
+with open('ecommerce.json') as fileptr:
+    data = json.load(fileptr)
+    while True:
+        sku = input("Please enter a product SKU: ")
+        if sku=='stop':
+            break
+        for product in data:
+            if product['sku'] == sku:
+                print("product found:")
+                print(product['name'])
+                print(product['price'])
+
